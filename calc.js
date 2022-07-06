67a4058842c04758b2addea94f1f3416a0ea9b3b
Kaitlyn Dolan <dolankaitlyn17@gmail.com>
2022-07-06 17:59:39 -0500

    final

--- calc.js
+++ calc.js
@@ -12,35 +12,81 @@ class Calculator{
     }
 
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
 
 //adding .toString() so that js will actually add the numbers, but we want to append them (display them on the screen)
 
     appendNumber(number){
-        if (number === '.' && this.currentOperand.includes('.')) return 
+        if (number === '.' && this.currentOperand.includes('.')) return //prevents decimals being used more than once 
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     chooseOperation(operation){
-        if (this.currentOperand == " ") return 
+        if (this.currentOperand === " ") return 
         if (this.previousOperand !== " "){
             this.compute()
         }
         this.operation = operation
         this.previousOperand = this.currentOperand
         this.currentOperand = ' '
-
     }
 
-    compute(){
+    compute() {
+        let computation 
+        const prev = parseFloat(this.previousOperand)
+        const current =  parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return 
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current 
+                break 
+            case '*':
+                computation = prev * current 
+                break 
+            case '/':
+                computation = prev / current
+                break
+            default: 
+            return
+        }
+        this.currentOperand = computation 
+        this.operation = undefined 
+        this.previousOperand = " "
+    }
 
+
+
+
+    getDisplayNumber(number){
+        const stringNumber = number.toString()
+        const integerDigits= parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ' '
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+            maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        }else{
+            return integerDisplay
+        }
     }
 
     updateDisplay(){
-        this.currentButton.innerText = this.currentOperand
-        this.previousButton.innerText = this.previousOperand
+        this.currentButton.innerText = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousButton.innerText = `${this.getDisplayNumber(this.previousOperand)}${this.operation}`
+        }else{
+            this.previousButton.innerText = ' '
+        }
     }
 }
 
@@ -73,4 +119,14 @@ operationButtons.forEach(button => {
 equalsButton.addEventListener('click', button => {
     calculator.compute()
     calculator.updateDisplay()
-)}
+})
+
+clearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
